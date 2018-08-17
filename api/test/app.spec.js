ace39bb9c47824bae35fc2942c760f1b3ebbbcc2
Oluwaseun Jolaoso <sudhons@gmail.com>
2018-08-17 19:43:43 +0100

    write tests for POST /api/v1/questions

--- api/test/app.spec.js
+++ api/test/app.spec.js
@@ -9,6 +9,7 @@ chai.use(chaiHttp);
 
 const testingQuestion1 = 'What is this?';
 const testingAnswer1 = 'It ia a ball';
+const emptyQuestion = '   ';
 
 describe('Server', () => {
   beforeEach((done) => {
@@ -71,6 +72,46 @@ describe('Server', () => {
           assert.strictEqual(res.body[0].answers[0].id, ansId);
           assert.isString(res.body[0].answers[0].answer);
           assert.deepEqual(res.body[0].answers[0].answer, testingAnswer1);
+          done();
+        });
+    });
+  });
+
+  describe('/POST /api/v1/questions', () => {
+    it('should not POST an empty question', (done) => {
+      chai.request(app)
+        .post('/api/v1/questions')
+        .send({ emptyQuestion })
+        .end((err, res) => {
+          assert.strictEqual(res.status, 422);
+          assert.isObject(res.body);
+          assert.isNotEmpty(res.body);
+          assert.hasAllKeys(res.body, ['message']);
+          assert.isString(res.body.message);
+          assert.strictEqual(res.body.message, 'Unsuccessful. Empty input field');
+          done();
+        });
+    });
+
+    it('should POST a question', (done) => {
+      chai.request(app)
+        .post('/api/v1/questions')
+        .send({ testingQuestion1 })
+        .end((err, res) => {
+          assert.strictEqual(res.status, 200);
+          assert.isObject(res.body);
+          assert.isNotEmpty(res.body);
+          assert.hasAllKeys(res.body, ['message', 'question']);
+          assert.isString(res.body.message);
+          assert.strictEqual(res.body.message, 'Question successfully posted');
+          assert.isObject(res.body.question);
+          assert.isNotEmpty(res.body.question);
+          assert.hasAllKeys(res.body.question, ['id', 'question', 'answers']);
+          assert.isString(res.body.question.id);
+          assert.isString(res.body.question.question);
+          assert.strictEqual(res.body.question.question, testingQuestion1);
+          assert.isArray(res.body.question.answers);
+          assert.isEmpty(res.body.question.answers);
           done();
         });
     });
