5f04a9547ebe2490ee2f212ec231abd2923a9de9
Oluwaseun Jolaoso <sudhons@gmail.com>
2018-08-17 21:07:58 +0100

    write tests for UPDATE /api/v1/questions/:id

--- api/test/app.spec.js
+++ api/test/app.spec.js
@@ -8,6 +8,7 @@ import data from '../dataStorage/data';
 chai.use(chaiHttp);
 
 const testingQuestion1 = 'What is this?';
+const testingQuestion2 = 'This is what?';
 const testingAnswer1 = 'It ia a ball';
 const emptyQuestion = '   ';
 const nonExistingQuestionId = '3bttte';
@@ -172,6 +173,99 @@ describe('App', () => {
           assert.lengthOf(res.body.answers, 1);
           assert.hasAllKeys(res.body.answers[0], ['id', 'answer']);
           assert.strictEqual(res.body.answers[0].answer, testingAnswer1);
+          done();
+        });
+    });
+  });
+
+  describe('/PUT /api/v1/questions/:id', () => {
+    it('should not UPDATE any question when the given question id does not exist', (done) => {
+      data.addQuestion(testingQuestion1);
+      chai.request(app)
+        .put(`/api/v1/questions/${nonExistingQuestionId}`)
+        .send({ question: testingQuestion2 })
+        .end((err, res) => {
+          assert.strictEqual(res.status, 404);
+          assert.isObject(res.body);
+          assert.isNotEmpty(res.body);
+          assert.hasAllKeys(res.body, ['message']);
+          assert.isString(res.body.message);
+          assert.strictEqual(res.body.message, `Unsuccessful. Question with id ${nonExistingQuestionId} is not found`);
+          done();
+        });
+    });
+
+    it('should not UPDATE with an empty question', (done) => {
+      const { id: qtnId } = data.addQuestion(testingAnswer1);
+      chai.request(app)
+        .put(`/api/v1/questions/${qtnId}`)
+        .send({ question: testingQuestion2 })
+        .end((err, res) => {
+          assert.strictEqual(res.status, 422);
+          assert.isObject(res.body);
+          assert.isNotEmpty(res.body);
+          assert.hasAllKeys(res.body, ['message']);
+          assert.isString(res.body.message);
+          assert.strictEqual(res.body.message, 'Unsuccessful. Empty input field');
+          done();
+        });
+    });
+
+    it('should UPDATE a question of a given id that is yet to have any answer', (done) => {
+      const { id: qtnId } = data.addQuestion(testingQuestion1);
+      chai.request(app)
+        .put(`/api/v1/questions/${qtnId}`)
+        .send({ question: testingQuestion2 })
+        .end((err, res) => {
+          assert.strictEqual(res.status, 200);
+          assert.isObject(res.body);
+          assert.isNotEmpty(res.body);
+          assert.hasAllKeys(res.body, ['message', 'question']);
+          assert.isString(res.body.message);
+          assert.strictEqual(res.body.message, 'Question successfully updated');
+          assert.isObject(res.body.question);
+          assert.isNotEmpty(res.body.question);
+          assert.hasAllKeys(res.body.question, ['id', 'question', 'answers']);
+          assert.isString(res.body.question.id);
+          assert.strictEqual(res.body.question.id, qtnId);
+          assert.isString(res.body.question.question);
+          assert.strictEqual(res.body.question.question, testingQuestion2);
+          assert.isArray(res.body.question.answers);
+          assert.isEmpty(res.body.question.answers);
+          done();
+        });
+    });
+
+    it('should UPDATE a question of the given id that has answers', (done) => {
+      const { id: qtnId } = data.addQuestion(testingQuestion1);
+      const { answers: [{ id: ansId }] } = data.addAnswer(qtnId, testingAnswer1);
+      chai.request(app)
+        .put(`/api/v1/questions/${qtnId}`)
+        .send({ question: testingQuestion2 })
+        .end((err, res) => {
+          assert.strictEqual(res.status, 200);
+          assert.isObject(res.body);
+          assert.isNotEmpty(res.body);
+          assert.lengthOf(Object.keys(res.body), 2);
+          assert.hasAllKeys(res.body, ['message', 'question']);
+          assert.isString(res.body.message);
+          assert.strictEqual(res.body.message, 'Question successfully updated');
+          assert.isObject(res.body.question);
+          assert.isNotEmpty(res.body.question);
+          assert.hasAllKeys(res.body.question, ['id', 'question', 'answers']);
+          assert.isString(res.body.question.id);
+          assert.strictEqual(res.body.question.id, qtnId);
+          assert.isString(res.body.question.question);
+          assert.strictEqual(res.body.question.question, testingQuestion2);
+          assert.isArray(res.body.question.answers);
+          assert.isNotEmpty(res.body.question.answers);
+          assert.lengthOf(res.body.question.answers, 1);
+          assert.isObject(res.body.question.answers[0]);
+          assert.hasAllKeys(res.body.question.answers[0], ['id', 'answer']);
+          assert.isString(res.body.question.answers[0].id);
+          assert.strictEqual(res.body.question.answers[0].id, ansId);
+          assert.isObject(res.body.question.answers[0].answer);
+          assert.strictEqual(res.body.question.answers[0].answer, testingAnswer1);
           done();
         });
     });
