e2a6acb340c3b4c88bbf21105bcb8306feb7a54a
Oluwaseun Jolaoso <sudhons@gmail.com>
2018-08-22 21:11:46 +0100

    fixed bug in nav

--- js/layout.js
+++ js/layout.js
@@ -1,12 +1,19 @@
 const navsForm = document.getElementById('header-search');
 const navsList = document.getElementById('header-nav');
-document.getElementById('menu').addEventListener('click', (e) => {
+
+
+const controlNav = (e) => {
   e.preventDefault();
-  if(navsForm.style.display==='none') {
+  
+  if(navsForm.style.display == 'none') {
     navsForm.style.display = 'block';
     navsList.style.display = 'block';
   } else {
     navsForm.style.display = 'none';
     navsList.style.display = 'none';
   }
-});
+};
+
+window.onload = controlNav;
+
+document.getElementById('menu').addEventListener('click', controlNav);
