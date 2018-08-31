0bbe095aff098aa83e471f0944330f5b9e88a19f
Oluwaseun Jolaoso <sudhons@gmail.com>
2018-08-31 03:15:33 +0100

    switches to online database

--- api/src/controllers/questionsController.js
+++ api/src/controllers/questionsController.js
@@ -1,7 +1,7 @@
 import { Client } from 'pg';
 import jwt from 'jsonwebtoken';
 
-const connectString = 'postgres://postgres:0099@localhost:5432/StackOverflow';
+const connectString = 'postgres://eopqezcwnocylf:49ffe41ef3606a1060ea80f39eae4291981c6e3c886b1b122517f1bc4dfb7faf@ec2-50-16-196-57.compute-1.amazonaws.com:5432/dcve49n4q71mpj?ssl=true';
 const client = new Client(connectString);
 
 client.connect();
